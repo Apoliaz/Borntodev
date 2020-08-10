2494a569e2d31c20974dc94653c9faf94b9d7766
Apoliaz <scasifer@gmai.com>
2020-08-10 11:52:05 +0700

    Create Exercise4_Jakkapan_P.py
    Exercise 4 : ปฏิบัติการทดสอบการใช้งานตัวแปร

---
+++ Exercise4_Jakkapan_P.py
@@ -0,0 +1,10 @@
+fe = 1.
+gb = 1.
+ic = 1.
+cp = 1.
+
+print("---Your Score---")
+print("Foundation English :",fe)
+print("General Business :",gb)
+print("Introduction to Computer Systems :",ic)
+print("Computer Programming :",cp)
